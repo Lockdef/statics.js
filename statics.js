0f91a7f9ce9825c5d195d475646b4cf0cfd6eab9
lockdef <locktki@icloud.com>
2019-05-13 06:17:15 +0900

    fix statics.js[remove sample]

--- statics.js
+++ statics.js
@@ -1,15 +1,13 @@
-sample = [1,2,3,4,5,6]
-
 let stats  = {
-    sum : (num_array) => {
+    sum : num_array => {
         let result = 0
         for (num of num_array) {
             result += num
         }
         return result
     },
-    avg : (num_array) => stats.sum(num_array) / num_array.length,
-    dev : (num_array) => {
+    avg : num_array => stats.sum(num_array) / num_array.length,
+    dev : num_array => {
         let avg = stats.avg(num_array)
         let result = []
         for (num of num_array){
@@ -17,12 +15,12 @@ let stats  = {
         }
         return result
     },
-    var : (num_array) => {
+    var : num_array => {
         dev_square = []
         for (num of stats.dev(num_array)){
             dev_square.push(num**2)
         }
         return (1 / num_array.length) * stats.sum(dev_square)
     },
-    std : (num_array) => Math.sqrt(stats.var(num_array))
+    std : num_array => Math.sqrt(stats.var(num_array))
 }
